8bb3b245d0d2d0ac1050cee22d60efa8ead71bbb
Bragin Boris <bragin.boris@ya.ru>
2022-04-15 13:26:10 +0300

    Added Kata binary.js

---
+++ binay.js
@@ -0,0 +1,13 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+      function binaryAddition(a, b) {
+        return (a + b).toString(2);
+      }
+
+      console.log(binaryAddition(1, 1));
+      console.log(binaryAddition(5, 9));
+
